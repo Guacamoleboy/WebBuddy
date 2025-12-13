e75fe7c0f369ab75de007a635f109e67836a5589
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-12-13 21:12:52 +0100

    Version 0.0.2

--- Project/Forms/MainForm.Designer.cs
+++ Project/Forms/MainForm.Designer.cs
@@ -7,11 +7,11 @@ using Project.VersionControl;
     partial class MainForm {
 
         // Attributes
-        private System.ComponentModel.IContainer components = null;
+        private System.ComponentModel.IContainer components = null; // Overall container for components
 
         // _______________________________________________________
   
-        protected override void Dispose(bool disposing) {
+        protected override void Dispose(bool disposing) { // Clears components
             if (disposing && (components != null)) {
                 components.Dispose();
             }
@@ -21,13 +21,19 @@ using Project.VersionControl;
         // _______________________________________________________
 
         private void InitializeComponent() {
-            SuspendLayout();
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
+
+            SuspendLayout(); // Layout Optimization
+
+            AutoScaleDimensions = new SizeF(7F, 15F); // DPI (NUMBER) & Font Scaling (F)
+            AutoScaleMode = AutoScaleMode.Font; // Makes sure it scales correct on all screen sizes
             ClientSize = new Size(800, 450); // GUI Size
             Text = $"WebBuddy | Version {Version.Current}"; // GUI Title
+            this.Icon = new Icon("Resources/Icon/logo-32.ico");
+
             Load += MainForm_Load;
-            ResumeLayout(false);
+
+            ResumeLayout(false); // Layout Optimization
+
         }
 
     }
--- Project/VersionControl/Version.cs
+++ Project/VersionControl/Version.cs
@@ -14,7 +14,7 @@ namespace Project.VersionControl {
     public static class Version {
 
         // Attributes
-        public const string Current = "0.0.1";
+        public const string Current = "0.0.2";
 
     }
 

dedb06a5bd9ad0acadbe6cf081cb5849d6c96641
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-12-13 20:42:23 +0100

    Version 0.0.1

---
+++ Project/Forms/MainForm.cs
@@ -0,0 +1,22 @@
+// Package
+namespace Project.Forms {
+
+    public partial class MainForm : Form { // : Form -> inherits from Form
+
+        // Attributes
+
+        // _______________________________________________________
+
+        public MainForm() { // Constructor
+            InitializeComponent();
+        }
+
+        // _______________________________________________________
+
+        private void MainForm_Load(object sender, EventArgs e) {
+
+        }
+
+    }
+
+}
---
+++ Project/VersionControl/Version.cs
@@ -0,0 +1,21 @@
+﻿// ________________________________________________________________________________________
+
+// PascalCase -> VariableName
+// camelCase -> variableName
+
+// If an attribute is constant it'll be applied with PascalCase in C#.
+// - Guac
+
+// ________________________________________________________________________________________
+
+// Package
+namespace Project.VersionControl {
+
+    public static class Version {
+
+        // Attributes
+        public const string Current = "0.0.1";
+
+    }
+
+}

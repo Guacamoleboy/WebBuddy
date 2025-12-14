9b3e80d9c88bff391c587273ca5a27afcf988b3d
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-12-14 02:23:09 +0100

    Version 0.0.3

--- Project/Forms/MainForm.cs
+++ Project/Forms/MainForm.cs
@@ -1,6 +1,10 @@
 // Package
 namespace Project.Forms {
 
+// Imports
+using Project.Theme;
+using Project.UI;
+
     public partial class MainForm : Form { // : Form -> inherits from Form
 
         // Attributes
@@ -9,11 +13,36 @@ namespace Project.Forms {
 
         public MainForm() { // Constructor
             InitializeComponent();
+
+            /*
+             
+            Custom TitleBar Attempt
+            _______________________
+
+            FormBorderStyle = FormBorderStyle.None;
+            Controls.Add(new TitleBar(this));
+
+            */
+
         }
 
         // _______________________________________________________
 
         private void MainForm_Load(object sender, EventArgs e) {
+
+            // Initial
+            var screen = Screen.PrimaryScreen.Bounds;
+            float scale = 0.6f; // 60% as float value
+
+            // Calculate and set height / width
+            int width = (int)(screen.Width * scale);
+            int height = (int)(screen.Height * scale);
+
+            // Sets app size dynamicly depending on screen size it's loaded on
+            this.Size = new Size(width, height);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            this.BackColor = ColorScheme.MainBlue;
 
         }
 
---
+++ Project/Theme/ColorScheme.cs
@@ -0,0 +1,26 @@
+﻿// Package
+namespace Project.Theme {
+
+    public static class ColorScheme {
+
+        // Colors
+        public static readonly Color MainBlue = Color.FromArgb(33, 24, 110);
+        public static readonly Color SecondBlue = Color.FromArgb(242, 29, 22, 94);
+        public static readonly Color Orange = Color.FromArgb(255, 177, 7);
+
+        // Text
+        public static readonly Color TextWhite = Color.FromArgb(255, 255, 255);
+        public static readonly Color TextPrimary = Color.FromArgb(230, 230, 230);
+        public static readonly Color TextSecondary = Color.FromArgb(105, 105, 105);
+
+        // Stroke
+        public static readonly Color MainBlueStroke = Color.FromArgb(32, 32, 32); // Change to correct value later
+        public static readonly Color SecondBlueStroke = Color.FromArgb(32, 32, 32); // Change to correct value later
+        public static readonly Color OrangeStroke = Color.FromArgb(32, 32, 32); // Change to correct value later
+
+        // Shadows
+        public static readonly Color DropShadowButton = Color.FromArgb(32, 32, 32); // Change to correct value later
+
+    }
+
+}
